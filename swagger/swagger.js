de0b27fec1374759c8c2b1c2374b0359f288f38b
Jon Desplats <jon.desplats@gmail.com>
2025-11-08 21:50:41 -0300

    Primer commitssks

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -22,8 +22,8 @@ const options = {
                 description: 'Servidor de desarrollo local'
             },
             {
-                url: 'https://api-alquiler-autos.herokuapp.com',
-                description: 'Servidor de producción'
+                url: 'https://deployefiback-production.up.railway.app',
+                description: 'Servidor de producción (Railway)'
             }
         ],
         components: {
